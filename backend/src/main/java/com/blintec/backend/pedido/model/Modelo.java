0a2bc2393c39ee3b925fc2423fd3bafcdcdea329
Andre Quintino <andre_quintino30@hotmail.com>
2026-07-22 21:12:41 -0300

    Cria entidade Modelo

---
+++ backend/src/main/java/com/blintec/backend/pedido/model/Modelo.java
@@ -0,0 +1,45 @@
+package com.blintec.backend.pedido.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "modelo")
+public class Modelo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String nome;
+
+    @Column(name = "consumo_tecido_por_peca", nullable = false, precision = 6, scale = 2)
+    private BigDecimal consumoTecidoPorPeca;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getConsumoTecidoPorPeca() {
+        return consumoTecidoPorPeca;
+    }
+
+    public void setConsumoTecidoPorPeca(BigDecimal consumoTecidoPorPeca) {
+        this.consumoTecidoPorPeca = consumoTecidoPorPeca;
+    }
+
+}
