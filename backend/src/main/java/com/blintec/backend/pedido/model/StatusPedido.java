a4ac087114799d9933670640d392ea3629cfd780
Andre Quintino <andre_quintino30@hotmail.com>
2026-07-22 21:12:41 -0300

    Cria enum StatusPedido

---
+++ backend/src/main/java/com/blintec/backend/pedido/model/StatusPedido.java
@@ -0,0 +1,11 @@
+package com.blintec.backend.pedido.model;
+
+public enum StatusPedido {
+    AGUARDANDO_PROGRAMACAO,
+    PROGRAMADO,
+    EM_CORTE,
+    EM_COSTURA,
+    EM_ARREMATACAO,
+    EM_EXPEDICAO,
+    ENTREGUE
+}
