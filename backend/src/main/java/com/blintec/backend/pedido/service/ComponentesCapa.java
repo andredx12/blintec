a03d74667a75004f8a7160d1abd9f8405ac438b2
Andre Quintino <andre_quintino30@hotmail.com>
2026-07-22 21:32:27 -0300

    Cria record ComponentesCapa

---
+++ backend/src/main/java/com/blintec/backend/pedido/service/ComponentesCapa.java
@@ -0,0 +1,10 @@
+package com.blintec.backend.pedido.service;
+
+public record ComponentesCapa(
+        int frentes,
+        int costas,
+        int ombrinhos,
+        int palas,
+        int abas
+) {
+}
