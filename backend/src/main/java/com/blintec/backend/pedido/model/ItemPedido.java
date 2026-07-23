e88405ca08af50db3b46ff1ad5ed2868482d32d3
Andre Quintino <andre_quintino30@hotmail.com>
2026-07-22 21:12:42 -0300

    Cria entidade ItemPedido

---
+++ backend/src/main/java/com/blintec/backend/pedido/model/ItemPedido.java
@@ -0,0 +1,56 @@
+package com.blintec.backend.pedido.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "item_pedido")
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id", nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "tamanho_modelo_id", nullable = false)
+    private TamanhoModelo tamanhoModelo;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public TamanhoModelo getTamanhoModelo() {
+        return tamanhoModelo;
+    }
+
+    public void setTamanhoModelo(TamanhoModelo tamanhoModelo) {
+        this.tamanhoModelo = tamanhoModelo;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+}
